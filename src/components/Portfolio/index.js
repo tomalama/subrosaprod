5a6a78d8013f41154d89222f0c5d79d78b412df7
tomalama <tommy.nguyen7@hotmail.com>
2019-03-26 19:21:41 -0400

    Added mobile friendly CSS

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,36 +6,54 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="Portfolio">
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://www.youtube.com/watch?v=murDG6qEFj4"
-          controls
-        />
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://www.youtube.com/watch?v=-I8OB9X6a6c"
-          controls
-        />
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://www.youtube.com/watch?v=IOXL-a_dzLg"
-          controls
-        />
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://www.youtube.com/watch?v=Z-DvMOWk_98"
-          controls
-        />
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://www.youtube.com/watch?v=y6ifesUvo1c"
-          controls
-        />
-        <ReactPlayer
-          className="Portfolio_player"
-          url="https://vimeo.com/214420886"
-          controls
-        />
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=murDG6qEFj4"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=-I8OB9X6a6c"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=IOXL-a_dzLg"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=Z-DvMOWk_98"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=y6ifesUvo1c"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="Portfolio_player-wrapper">
+          <ReactPlayer
+            url="https://vimeo.com/214420886"
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
     );
   }
