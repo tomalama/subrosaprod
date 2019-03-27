a5588ac87f1ce33bf9c3f2c7b15828188b1cd0c1
tomalama <tommy.nguyen7@hotmail.com>
2019-03-26 21:30:34 -0400

    Added more mobile friendly CSS

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,37 +6,38 @@ import logo from "../../assets/logo.png";
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav className="Navbar">
-          <div className="Navbar_header">
-            <Link to={"/"}>
-              <img className="Navbar_logo" src={logo} alt="Logo" />
-            </Link>
+      <div className="Navbar">
+        <div className="Navbar_header">
+          <Link to={"/"}>
+            <img className="Navbar_logo" src={logo} alt="Logo" />
+          </Link>
+          <div className="Navbar_link-toggle">
+            <i className="fas fa-bars" />
           </div>
-          <div className="Navbar_content">
-            <ul className="Navbar_links">
-              <li>
-                <Link to={"/demo-reel"} className="Navbar_link">
-                  Demo Reel
-                </Link>
-              </li>
-              <li>
-                <Link to={"/portfolio"} className="Navbar_link">
-                  Portfolio
-                </Link>
-              </li>
-              <li>
-                <Link to={"/about"} className="Navbar_link">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to={"/contact"} className="Navbar_link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
+        </div>
+        <nav className="Navbar_content">
+          <ul className="Navbar_links">
+            <li>
+              <Link to={"/demo-reel"} className="Navbar_link">
+                Demo Reel
+              </Link>
+            </li>
+            <li>
+              <Link to={"/portfolio"} className="Navbar_link">
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to={"/about"} className="Navbar_link">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} className="Navbar_link">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </nav>
       </div>
     );
